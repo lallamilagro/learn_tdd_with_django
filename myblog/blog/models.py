75624eb781dc85e3be9622a1254ad0fcaf613270
lallamilagro <for.lenochka@gmail.com>
2017-10-27 14:12:41 +1000

    Test: Entrys

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -6,6 +6,9 @@ class Entry(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     modified_at = models.DateTimeField(auto_now=True, editable=False)
+    
+    class Meta:
+        verbose_name_plural = "entries"
 
     def __str__(self):
         return self.title
