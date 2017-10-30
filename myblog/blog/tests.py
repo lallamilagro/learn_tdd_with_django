d905977e1bebc2239298ae14936d569f1c281bdf
lallamilagro <for.lenochka@gmail.com>
2017-10-30 14:43:06 +1000

    Test for blog entries show up on the homepage - fails

--- myblog/blog/tests.py
+++ myblog/blog/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Entry
+from django.contrib.auth import get_user_model
 
 class EntryModelTest(TestCase):
 
@@ -16,3 +17,24 @@ class ProjectTests(TestCase):
     def test_homepage(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+
+class HomePageTests(TestCase):
+    """Test whether out blog entries show up on the homepage"""
+    def setUp(self):
+        self.user = get_user_model().objects.create(username='some_user')
+
+    def test_one_entry(self):
+        Entry.objects.create(title='1-title', body='1-body', author=self.user)
+        response = self.client.get('/')
+        self.assertContains(response, '1-title')
+        self.assertContains(response, '1-body')
+
+    def test_two_entries(self):
+        Entry.objects.create(title='1-title', body='1-body', author=self.user)
+        Entry.objects.create(title='2-title', body='2-body', author=self.user)
+        response = self.client.get('/')
+        self.assertContains(response, '1-title') 
+        self.assertContains(response, '1-body') 
+        self.assertContains(response, '2-title') 
+         
