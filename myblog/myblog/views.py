807feb79b240f6998ce3edfe1856f5c2b109d907
lallamilagro <for.lenochka@gmail.com>
2017-10-30 16:03:10 +1000

    Update homepage template

--- myblog/myblog/views.py
+++ myblog/myblog/views.py
@@ -1,5 +1,9 @@
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+
+from blog.models import Entry
+
 
 class HomeView(TemplateView):
-
     template_name = 'index.html'
+    queryset = Entry.objects.order_by('-created_at')
