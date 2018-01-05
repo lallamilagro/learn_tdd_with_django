6d65c663a8c835e77e2ca501bb337e3d06b9a0d4
lallamilagro <for.lenochka@gmail.com>
2018-01-06 00:03:51 +1000

    Defined a get_absolute_url method, tests passes

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Entry(models.Model):
     title = models.CharField(max_length=500)
@@ -12,3 +13,6 @@ class Entry(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('entry_detail', kwargs={'pk': self.pk})
