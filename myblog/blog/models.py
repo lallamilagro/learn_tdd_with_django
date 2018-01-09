cc7ed25137a8921bfd95a8f4a00fa933cfa6c41e
lallamilagro <for.lenochka@gmail.com>
2018-01-09 16:55:55 +1000

    Create a str method, tests passes

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -26,4 +26,5 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     modified_at = models.DateTimeField(auto_now=True, editable=False)
 
-    
+    def __str__(self):
+        return self.body
