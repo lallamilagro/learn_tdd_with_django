73787eb4a7195e315f705ea24929e8ca19c9ab39
lallamilagro <for.lenochka@gmail.com>
2018-01-09 16:47:56 +1000

    Add a test for comment model

--- myblog/blog/tests.py
+++ myblog/blog/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Entry
+from .models import Entry, Comment
 from django.contrib.auth import get_user_model
 
 class EntryModelTest(TestCase):
@@ -15,6 +15,13 @@ class EntryModelTest(TestCase):
         user = get_user_model().objects.create(username='some_user')
         entry = Entry.objects.create(title="My entry title", author=user)
         self.assertIsNotNone(entry.get_absolute_url())
+
+
+class CommentModelTest(TestCase):
+
+    def test_string_representation(self):
+        comment = Comment(body="My comment body")
+        self.assertEqual(str(comment), "My comment body")
 
 
 class ProjectTests(TestCase):
